de521a851ac4342d62d0df200a5019624b8af09e
1like2learn <ndespained@gmail.com>
2021-01-11 15:18:16 -0800

    Makes transition smoother

--- src/components/imageCont.js
+++ src/components/imageCont.js
@@ -1,28 +1,26 @@
 import React, { useState } from "react";
-
+/**
+ * Displays two versions of an image and displays the appropriate version depending
+ * on the state of the higher quality one.
+ */
 export default function ImageCont() {
-    // Where we store the state of the images
+    // Where we store the loaded state of the images
     const [ imageLoaded, setImageLoaded ] = useState(false);
 
-    // When the fullsize image loads, setImageLoaded sets imageLoaded to true. 
-    // The ternary operators flip the display properties.
+    // When the full size image loads, setImageLoaded sets imageLoaded to true. 
+    // The ternary operators in the className properties change the class names
+    // of the images, causing the thumbnail to disappear and the full version to appear.
     return (
         <>
             <img 
-                className = "genericImage"
+                className = {`genericImage image${imageLoaded? "Inv": "Vis"}`}
                 src = "https://forge-homework.s3.amazonaws.com/thumb.jpg" 
                 alt = "Istanbul street corner. Photographer: Sanaan Mazhar"
-                style = {{
-                    display: imageLoaded? "none": "block",
-                }}
             />
             <img 
-                className = "genericImage"
+                className = {`genericImage image${imageLoaded? "Vis": "Inv"}`}
                 src = "https://forge-homework.s3.amazonaws.com/fullsize.jpg" 
                 alt = "Istanbul street corner. Photographer: Sanaan Mazhar"
-                style = {{
-                    display: imageLoaded? "block": "none",
-                }}
                 onLoad = { () => setImageLoaded(true)}
             />
         </>
