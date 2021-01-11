11c0dfcd485bc1d4829d2d8f22fc86634d4c9c8d
BuildTools <unconfigured@null.spigotmc.org>
2021-01-11 13:06:20 -0800

    creates imageCont

---
+++ src/components/imageCont.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+export default function ImageCont() {
+    // Where we store the state of the images
+    const [ imageLoaded, setImageLoaded ] = useState(false);
+
+    // When the fullsize image loads, 
+    return (
+        <>
+            <img 
+                src = "https://forge-homework.s3.amazonaws.com/thumb.jpg" 
+                alt = "Istanbul street corner. Photographer: Sanaan Mazhar"
+                style = {{display: imageLoaded? "none":"block"}}
+            />
+            <img 
+                src = "https://forge-homework.s3.amazonaws.com/fullsize.jpg" 
+                alt = "Istanbul street corner. Photographer: Sanaan Mazhar"
+                style = {{display: imageLoaded? "block":"none"}}
+                onLoad = { () => setImageLoaded(true)}
+            />
+        </>
+    )
+}
