16ba75d91b64b0d9356accc29ed898640c2206a7
BuildTools <unconfigured@null.spigotmc.org>
2021-01-11 13:10:45 -0800

    fixes logic, adds comments

--- src/components/imageCont.js
+++ src/components/imageCont.js
@@ -4,18 +4,19 @@ export default function ImageCont() {
     // Where we store the state of the images
     const [ imageLoaded, setImageLoaded ] = useState(false);
 
-    // When the fullsize image loads, 
+    // When the fullsize image loads, setImageLoaded sets imageLoaded to true. 
+    // The ternary operators flip the display properties.
     return (
         <>
             <img 
                 src = "https://forge-homework.s3.amazonaws.com/thumb.jpg" 
                 alt = "Istanbul street corner. Photographer: Sanaan Mazhar"
-                style = {{display: imageLoaded? "none":"block"}}
+                style = {{display: imageLoaded? "none": "block"}}
             />
             <img 
                 src = "https://forge-homework.s3.amazonaws.com/fullsize.jpg" 
                 alt = "Istanbul street corner. Photographer: Sanaan Mazhar"
-                style = {{display: imageLoaded? "block":"none"}}
+                style = {{display: imageLoaded? "block": "none"}}
                 onLoad = { () => setImageLoaded(true)}
             />
         </>
