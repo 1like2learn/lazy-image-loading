7d6e144f89b809ab76c5ea577c904ea661bc11bc
BuildTools <unconfigured@null.spigotmc.org>
2021-01-11 13:06:04 -0800

    cleans up app, imports imageCont

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import ImageCont from "./components/imageCont";
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <ImageCont />
+    </div>
+  );
+}
+
+export default App;
